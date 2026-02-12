1fa6ec001dfa427a0e468d85dbf4f9bbb8df8851
M. Elijah Wangeman <maw4678@rit.edu>
2026-02-11 20:54:07 -0500

    Initial Commit

---
+++ ilo sitelen/ilo_sitelenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ilo_sitelenApp.swift
+//  ilo sitelen
+//
+//  Created by koteczek on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct ilo_sitelenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
